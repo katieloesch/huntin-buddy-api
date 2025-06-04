11361b1e18d653f436e1a858a86d375e06aed883
Katie Loesch <catherine.loesch@protonmail.com>
2025-06-04 12:04:15 +0100

    modify server.js for deployment

--- server.js
+++ server.js
@@ -45,16 +45,16 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  // handle cors issues
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-  next();
-});
+// app.use((req, res, next) => {
+//   // handle cors issues
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//   );
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+//   next();
+// });
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
